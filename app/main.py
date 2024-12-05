658c0fe14c36c343c1c679808d6cbefd21e657f9
Douglas Rossetto <douglas.rossetto@willbank.com.br>
2024-12-05 15:59:41 -0300

    ajustando configs

--- app/main.py
+++ app/main.py
@@ -2,19 +2,28 @@ from pyspark.sql import SparkSession
 import boto3
 
 # Configurar a SparkSession
-spark = SparkSession.builder \
-    .appName("Spark AWS Test") \
-    .config("spark.hadoop.fs.s3a.access.key", "AWS_ACCESS_KEY_ID") \
-    .config("spark.hadoop.fs.s3a.secret.key", "AWS_SECRET_ACCESS_KEY") \
-    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
-    .getOrCreate()
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder \
+    .appName("S3 Access") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", 
+            "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
+    .getOrCreate()
+    
 # Ler da camada raw
-raw_path = "s3a://bucket-raw/path-to-data/"
-data = spark.read.json(raw_path)
+raw_path = "s3a://pacman-data-analytics-stream-prod/success/source=plataforma-cobranca-distribuicao/year=2024/month=12/day=05/*/"
+data = spark.read.parquet(raw_path)
 
 # Processar e gravar no sandbox
 sandbox_path = "s3a://bucket-sandbox/path-to-data/"
 data.write.mode("overwrite").parquet(sandbox_path)
 
 print("Processamento concluído!")
+
+
+
+import boto3
+s3 = boto3.client('s3')
+response = s3.list_objects_v2(Bucket='pacman-data-analytics-stream-prod')
+print(response)
